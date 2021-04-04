71481841b6af432200c1265e346de065a7e6b7fb
Akiacode <catry.me@gmail.com>
2021-04-04 20:32:27 +0900

    welcome

--- apis/mod.js
+++ apis/mod.js
@@ -1,4 +1,12 @@
 async function handleRequest(request) {
+
+    return new Response(`
+        <h1>Welcome to Open apis</h1>
+        <h3><a href="https://github.com/AkiaCode/deno-deploy/issues/new">If you want to add another api, please create an issue</a></h3>
+    `, {
+        headers: {},
+        status: 200
+    })
 }
 
 addEventListener("fetch", (event) => {
