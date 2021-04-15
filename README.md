9bbf042583698860621e4e89c07e7bb30a0ae9c1
Akiacode <catry.me@gmail.com>
2021-04-15 23:59:29 +0900

    idk

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * ./tools/mod.js - https://tools.deno.dev/
 * ./apis/mod.js - https://apis.deno.dev/
 * ./javalab/mod.js - https://javalab.deno.dev/
+* ./nsfw/mod.js - https://nsfw.deno.dev/
---
+++ nsfw/mod.js
@@ -0,0 +1,22 @@
+async function handleRequest(request) {
+
+    const response = await fetch("https://h_api.pmh.codes/gallery/1806299", {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+          'Accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8'
+        },
+      })
+
+    const { files }  = await response.json();
+    const test = await files.map(x => "<img src=\"" + x.url.replace("http://127.0.0.1:8270/", "https://h_api.pmh.codes/") + "\" style=\"display: block; margin: 0 auto; width: 450px; height: 450px;\">");
+
+    return new Response(`<body>${test.join("\n")}</body>
+    `, {
+        headers: {},
+        status: 200
+    })
+}
+
+addEventListener("fetch", (event) => {
+    event.respondWith(handleRequest(event.request))
+});
